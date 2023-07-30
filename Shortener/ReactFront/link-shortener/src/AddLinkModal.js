76a3c4020f374a5ee8cd4df5d355073f25d544e4
Liubomyr <yupiter2101@gmail.com>
2023-07-30 23:38:29 +0300

    link redirect added

--- Shortener/ReactFront/link-shortener/src/AddLinkModal.js
+++ Shortener/ReactFront/link-shortener/src/AddLinkModal.js
@@ -30,45 +30,43 @@ export class AddLinkModal extends Component{
     render(){
         return (
             <div className="container">
+                <Modal
+                {...this.props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                >
+                    <Modal.Header clooseButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            Add Link
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
 
-<Modal
-{...this.props}
-size="lg"
-aria-labelledby="contained-modal-title-vcenter"
-centered
->
-    <Modal.Header clooseButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-            Add Link
-        </Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
+                        <Row>
+                            <Col sm={6}>
+                                <Form onSubmit={this.handleSubmit}>
+                                    <Form.Group controlId="LongLink">
+                                        <Form.Label>Enter your link</Form.Label>
+                                        <Form.Control type="text" name="BaseLink" required 
+                                        placeholder="https://www..."/>
+                                    </Form.Group>
 
-        <Row>
-            <Col sm={6}>
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group controlId="LongLink">
-                        <Form.Label>Enter your link</Form.Label>
-                        <Form.Control type="text" name="BaseLink" required 
-                        placeholder="https://www..."/>
-                    </Form.Group>
-
-                    <Form.Group>
-                        <Button variant="primary" type="submit">
-                            Send
-                        </Button>
-                    </Form.Group>
-                </Form>
-            </Col>
-        </Row>
-    </Modal.Body>
-    
-    <Modal.Footer>
-        <Button variant="danger" onClick={this.props.onHide}>Close</Button>
-    </Modal.Footer>
-
-</Modal>
-
+                                    <Form.Group>
+                                        <Button variant="primary" type="submit">
+                                            Send
+                                        </Button>
+                                    </Form.Group>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </Modal.Body>
+                    
+                    <Modal.Footer>
+                        <Button variant="danger" onClick={this.props.onHide}>Close</Button>
+                    </Modal.Footer>
+                    
+                </Modal>
             </div>
         )
     }
